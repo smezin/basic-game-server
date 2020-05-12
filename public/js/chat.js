946590da62b0ee7369d52405afd64ed747f0beca
Shahaf Mezin <smezin@gmail.com>
2020-05-12 20:14:02 +0300

    down to the very basic

--- public/js/chat.js
+++
@@ -1,9 +0,0 @@
-const socket = io()
-
-//options
-
-socket.on('connect', (message) => {
-    console.log('im the client, hello')
-    socket.emit('hello', 'Im connected')
-    })
-
--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
-const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-const cors = require('cors')
 
 app = express()
-app.use(cors())
 const server = http.createServer(app)
 const io = socketio(server);
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
-
- app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket)=>{
     console.log('new web socket connection')
-    socket.emit('packet', 'packet from server')
+    socket.emit('hello', 'welcome')
 
     socket.on('hello', (msg)=>{
         console.log(`im the server, got your "${msg}"`)
     })
 
- 
     socket.on('disconnect', () => {
        console.log('byebye')
     })
 })
-
 
 server.listen(port, ()=>{
     console.log(`server is up on ${port}!`)
