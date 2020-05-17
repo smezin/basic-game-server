9772ba79a02807e95aefcd5a6e2ff4cd8eed4ed2
Shahaf Mezin <smezin@gmail.com>
2020-05-17 15:05:36 +0300

    checkpoint b4 refactoring requests files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,34 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/game-users', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-})
-
-const User = mongoose.model('User', {
-    user: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        minlength: 1,
-        maxlength: 12
-    },
-    level: {
-        type: String
-    }
-
-})
-
-const me = new User({
-    user: '  shaHaf',
-    level: 'expert'
-})
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
---
+++ src/models/user.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+        minlength: 3,
+        maxlength: 12
+    },
+    password: {
+        type: String,
+        trim: true,
+        minlength: 6
+    },
+    wins: {
+        type: Number,
+        default: 1
+    },
+    loses: {
+        type: Number,
+        default: 1
+    },
+    tokens: [{
+        token: {
+            type: String,
+            //required: true
+        }
+    }]
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+};
+
+userSchema.statics.findByCredentials = async (userName, password) => {
+    const user = await User.findOne({ userName });
+    if (!user) {
+        throw new Error('error: unable to login');
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+        throw new Error('error: unable to login');
+    }
+
+    return user;
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
