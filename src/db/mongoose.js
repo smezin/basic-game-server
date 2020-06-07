2dedc2287a0133930ad9d3e487376709a92cf8d8
Shahaf Mezin <smezin@gmail.com>
2020-06-07 13:09:14 +0300

    docker compose fixes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //mongodb://mongo:27017/
-mongoose.connect('mongodb://mongo/game-users', {
+mongoose.connect('mongodb://mongo:27017/game-users', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
